403ab1189557adf41269861b7f9bff54d46c41bd
Thanh Bui Minh <thanhbuiminh@Thanhs-MacBook-Pro.local>
2019-05-20 08:42:59 +0700

    + add script reporting coverage to codebeat

---
+++ codebeat.sh
@@ -0,0 +1,3 @@
+export CODECLIMATE_API_HOST=https://codebeat.co/webhooks/code_coverage
+export CODECLIMATE_REPO_TOKEN=a8130f6b-982d-4836-8b50-03830dd7db5c
+./node_modules/.bin/codeclimate-test-reporter < coverage/lcov.info
